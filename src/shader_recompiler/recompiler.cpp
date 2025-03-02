a39deb6851b443d328f550546282e081124654d6
Lander Gallastegi <landergallastegi@gmail.com>
2025-03-03 00:04:19 +0100

    clang-format

--- src/shader_recompiler/recompiler.cpp
+++ src/shader_recompiler/recompiler.cpp
@@ -5,9 +5,9 @@
 #include "common/io_file.h"
 #include "common/path_util.h"
 #include "shader_recompiler/frontend/control_flow_graph.h"
-#include "shader_recompiler/ir/conditional_tree.h"
 #include "shader_recompiler/frontend/decode.h"
 #include "shader_recompiler/frontend/structured_control_flow.h"
+#include "shader_recompiler/ir/conditional_tree.h"
 #include "shader_recompiler/ir/passes/ir_passes.h"
 #include "shader_recompiler/ir/post_order.h"
 #include "shader_recompiler/recompiler.h"
@@ -62,7 +62,7 @@ IR::Program TranslateProgram(std::span<const u32> code, Pools& pools, Info& info
                                                 program.info, runtime_info, profile);
     program.blocks = GenerateBlocks(program.syntax_list);
     program.post_order_blocks = Shader::IR::PostOrder(program.syntax_list.front());
-    
+
     Shader::IR::AddConditionalTreeFromASL(program.syntax_list);
 
     // Run optimization passes
